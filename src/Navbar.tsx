49796118a5cefd148a65df41ae16307e8f23f7fe
Ivan Culina <ivan.culina-ext@aviv-group.com>
2024-11-08 16:00:06 +0100

    improve logout look

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -1,7 +1,9 @@
 import React from "react";
-import { Navbar, Nav } from "react-bootstrap";
+import { Navbar, Nav, Dropdown } from "react-bootstrap";
 import { Link, useNavigate } from "react-router-dom";
 import { useAuth } from "./AuthContext";
+import { jwtDecode } from "jwt-decode";
+import { FaUser } from "react-icons/fa";
 
 const AppNavbar: React.FC = () => {
   const navigate = useNavigate();
@@ -12,6 +14,16 @@ const AppNavbar: React.FC = () => {
     navigate("/login");
   };
 
+  let email = "";
+
+  if (isLoggedIn) {
+    const jwtToken = localStorage.getItem("jwtToken");
+    if (jwtToken) {
+      const decoded: any = jwtDecode(jwtToken);
+      email = decoded.email || "";
+    }
+  }
+
   return (
     <Navbar bg="dark" variant="dark" expand="lg">
       <Navbar.Brand as={Link} to="/">
@@ -19,25 +31,36 @@ const AppNavbar: React.FC = () => {
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="ml-auto">
-          {isLoggedIn ? (
-            <>
+        {isLoggedIn ? (
+          <>
+            <Nav>
               <Nav.Link as={Link} to="/todos">
                 Todos
               </Nav.Link>
-              <Nav.Link onClick={handleLogout}>Logout</Nav.Link>
-            </>
-          ) : (
-            <>
-              <Nav.Link as={Link} to="/login">
-                Login
-              </Nav.Link>
-              <Nav.Link as={Link} to="/register">
-                Register
-              </Nav.Link>
-            </>
-          )}
-        </Nav>
+            </Nav>
+            <Nav className="ms-auto">
+              <Dropdown as={Nav.Item}>
+                <Dropdown.Toggle as={Nav.Link} id="profile-dropdown">
+                  <FaUser />
+                </Dropdown.Toggle>
+                <Dropdown.Menu align="end">
+                  <Dropdown.ItemText>{email}</Dropdown.ItemText>
+                  <Dropdown.Divider />
+                  <Dropdown.Item onClick={handleLogout}>Logout</Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+            </Nav>
+          </>
+        ) : (
+          <Nav className="ms-auto">
+            <Nav.Link as={Link} to="/login">
+              Login
+            </Nav.Link>
+            <Nav.Link as={Link} to="/register">
+              Register
+            </Nav.Link>
+          </Nav>
+        )}
       </Navbar.Collapse>
     </Navbar>
   );
